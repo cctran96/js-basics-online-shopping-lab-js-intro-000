80d617d034fccb57baa5c715c131066ea77518d8
Chau Tran <cctran96@gmail.com>
2021-02-23 01:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,33 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ let newObject = {itemName:`${item}`, itemPrice: Math.floor(Math.random() * 101)}
+ cart.push(newObject)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    let l1 = []
+    for (let item in cart[0]) {
+      l1.push(cart[0][item])
+    }
+    return `In your cart, you have ${l1[0]} at $${l1[1]}.`
+  } else {
+    let str = `In your cart, you have `
+    let l2 = []
+    for (let i = 0; i < cart.length; i++) {
+      for (let item in cart[i]) {
+      l2.push(cart[i][item])
+      }
+      for (let x = 0; x < l2.length - 1; x += 2) {
+        str += `${l2[x]} at $${l2[x + 1]}`
+      }
+    }
+    return str + "."
+  }
 }
 
 function total() {
